267a3a0051af9e4f3fddaec09a0e72fe5272b454
kawgit <kawgit56@gmail.com>
2026-04-27 23:20:16 -0400

    tweak

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #$ -N train_v0
 #$ -l h_rt=10:00:00
 #$ -pe omp 1
-#$ -l mem_per_core=4G
+#$ -l mem_per_core=8G
 #$ -j y
 #$ -o /usr4/cs440/kawgit56/risk/logs
 
@@ -16,4 +16,4 @@ javac -cp "./lib/*:src" @risk.srcs || {
 }
 
 # Train
-java -cp "./lib/*:src" edu.bu.pas.risk.SequentialTrain pas.risk.agent.RiskQAgent random -x 100 -g .9 -t 10 -v 1 2>&1
+java -cp "./lib/*:src" edu.bu.pas.risk.SequentialTrain pas.risk.agent.RiskQAgent random -x 100 -g .9 -t 100 -v 10 2>&1
